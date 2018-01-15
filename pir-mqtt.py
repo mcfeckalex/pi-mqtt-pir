2bbdf3f3a2e0f177b030e321a870da1f4dc94629
albe <alex.bstrom@gmail.com>
2018-01-15 11:33:58 +0000

    Fixed path error so dropbox upload failed. Added logging to log file pir_mqtt.log

--- pir-mqtt.py
+++ pir-mqtt.py
@@ -5,6 +5,12 @@ import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 from subprocess import call
 from subprocess import check_output
+import logging
+#logging setup
+LOG_LEVEL = logging.DEBUG
+LOG_FILE = "/home/pi/mqtt/mqtt-pir/pir_mqtt.log"
+LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
+logging.basicConfig(filename=LOG_FILE, format=LOG_FORMAT, level=LOG_LEVEL)
 
 #GPIO setup
 GPIO.setmode(GPIO.BCM)
@@ -19,7 +25,6 @@ uploader_path =  "/home/pi/Dropbox-Uploader/dropbox_uploader.sh"
 db_cam_path = "/pi_cam/"
 script_path = os.path.dirname(os.path.realpath(__file__))+"/"
 
-
 broker = '192.168.1.80'
 state_topic = 'home-assistant/pir/state'
 delay=1
@@ -27,7 +32,6 @@ delay=1
 def db_if_exists(filename, path = db_cam_path+cam_location):
    print uploader_path
    list = check_output([uploader_path, "list", path])
-   print list
    if filename in list:
       return True
    else:
@@ -37,28 +41,32 @@ off_sent = False
 input_state = False
 #pir loop:-
 while True:   
-   print "start wait.."
+   logging.debug("Waiting for motion...")
+
    while input_state == False:
       input_state = GPIO.input(18)
    input_state = False
+   logging.debug("motion detected...")
    curr_date = time.strftime('%y-%m-%d')
    time_now = time.strftime('%y-%m-%d-%H%M%S')
    db_snap_path = db_cam_path+cam_location+curr_date
    snap_filename = db_snap_path+"/"+time_now+".jpeg"
    publish.single(state_topic, 'on', hostname=broker)
-   call(["wget", "http://raspberrypi:8080/stream/snapshot.jpeg?delay_s=3", "-O", time_now+".jpeg"])
+   logging.debug("downloading snapshot..." )
+   call(["wget", "http://raspberrypi:8080/stream/snapshot.jpeg?delay_s=3", "-O", script_path+time_now+".jpeg"])
    if db_if_exists(curr_date):
       call([uploader_path, "upload", script_path+time_now+".jpeg", snap_filename])
    else:
       call([uploader_path, "mkdir", db_snap_path])
       call([uploader_path, "upload", script_path+time_now+".jpeg", snap_filename])
    
-   dir_name = "/home/pi/mqtt/mqtt-pir/"
-   test = os.listdir(dir_name)
-
+   
+   test = os.listdir(script_path)
+   logging.debug("Files in script dir:\n"+str(test))
    for item in test:
       if item.endswith(".jpeg"):
-         os.remove(os.path.join(dir_name, item))
+         logging.debug("removing snapshot "+os.path.join(script_path, item))
+         os.remove(os.path.join(script_path, item))
    time.sleep(delay)
-   
+
    publish.single(state_topic, 'off', hostname=broker)
